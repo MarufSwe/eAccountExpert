535f151fc6b1e8117068254eef6a6e83680a2eb1
belal-et <belal@esquiretechnology.com>
2025-06-18 11:23:45 +0600

    contact info update

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from landingpage.views.landingpage_views import *
+from landingpage.views.contact_views import *
 urlpatterns = [
  path('', landing_page, name='landing-page'),
+ path('contact', contact_views, name='contact-page'),
 ]
